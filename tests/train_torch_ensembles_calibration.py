f761567ebfafabeb2b96ee881040a2f653fc8234
Jean-Bernard Hayet <jbhayet@cimat.mx>
2022-03-25 23:46:13 -0600

    Added MAIN

--- tests/train_torch_ensembles_calibration.py
+++ tests/train_torch_ensembles_calibration.py
@@ -57,39 +57,10 @@ args = parser.parse_args()
 
 
 
-logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
-# GPU
-if torch.cuda.is_available():
-    logging.info(torch.cuda.get_device_name(torch.cuda.current_device()))
-
-# Load the default parameters
-experiment_parameters = Experiment_Parameters(add_kp=False,obstacles=False)
-
-dataset_dir   = "datasets/"
-dataset_names = ['eth-hotel','eth-univ','ucy-zara01','ucy-zara02','ucy-univ']
-idTest        = 2
-pickle        = True
-
-# parameters models
-num_ensembles  = 1
-
-# Load the dataset and perform the split
-training_data, validation_data, test_data, test_homography = setup_loo_experiment('ETH_UCY',dataset_dir,dataset_names,idTest,experiment_parameters,pickle_dir='pickle',use_pickled_data=args.pickle)
-
-# Torch dataset
-train_data = traj_dataset(training_data['obs_traj_rel'], training_data['pred_traj_rel'],training_data['obs_traj'], training_data['pred_traj'])
-val_data   = traj_dataset(validation_data['obs_traj_rel'], validation_data['pred_traj_rel'],validation_data['obs_traj'], validation_data['pred_traj'])
-test_data  = traj_dataset(test_data['obs_traj_rel'], test_data['pred_traj_rel'], test_data['obs_traj'], test_data['pred_traj'])
-
-# Form batches
-batched_train_data = torch.utils.data.DataLoader(train_data,batch_size=args.batch_size,shuffle=False)
-batched_val_data   = torch.utils.data.DataLoader(val_data,batch_size=args.batch_size,shuffle=False)
-batched_test_data  = torch.utils.data.DataLoader(test_data,batch_size=args.batch_size,shuffle=False)
-
 import torch.optim as optim
 
 # Function to train the models
-def train(model,ind,idTest):
+def train(model,device,ind,idTest,train_data,val_data):
     # Optimizer
     # optimizer = optim.SGD(model.parameters(), lr=initial_lr)
     optimizer = optim.Adam(model.parameters(),lr=args.learning_rate, betas=(.5, .999),weight_decay=0.8)
@@ -103,7 +74,7 @@ def train(model,ind,idTest):
         error = 0
         total = 0
         # Recorremos cada batch
-        for batch_idx, (data, target, data_abs , target_abs) in enumerate(batched_train_data):
+        for batch_idx, (data, target, data_abs , target_abs) in enumerate(train_data):
             # Remember that Pytorch accumulates gradients.
             # We need to clear them out before each instance
             model.zero_grad()
@@ -127,7 +98,7 @@ def train(model,ind,idTest):
         # Validation
         error = 0
         total = 0
-        for batch_idx, (data_val, target_val, data_abs , target_abs) in enumerate(batched_val_data):
+        for batch_idx, (data_val, target_val, data_abs , target_abs) in enumerate(val_data):
 
             if torch.cuda.is_available():
               data_val  = data_val.to(device)
@@ -155,138 +126,164 @@ def train(model,ind,idTest):
     plt.ylabel("Loss")
     plt.legend()
 
-# Model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Seleccionamos de forma aleatorea las semillas
-seeds = np.random.choice(99999999, num_ensembles , replace=False)
-print("Seeds: ", seeds)
+def main():
+    # Printing parameters
+    torch.set_printoptions(precision=2)
+
+    # Device
+    if torch.cuda.is_available():
+        logging.info(torch.cuda.get_device_name(torch.cuda.current_device()))
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
+    # Load the default parameters
+    experiment_parameters = Experiment_Parameters(add_kp=False,obstacles=False)
+
+    dataset_dir   = "datasets/"
+    dataset_names = ['eth-hotel','eth-univ','ucy-zara01','ucy-zara02','ucy-univ']
+    idTest        = 2
+    pickle        = True
+
+    # parameters models
+    num_ensembles  = 1
+
+    # Load the dataset and perform the split
+    training_data, validation_data, test_data, test_homography = setup_loo_experiment('ETH_UCY',dataset_dir,dataset_names,idTest,experiment_parameters,pickle_dir='pickle',use_pickled_data=args.pickle)
+
+    # Torch dataset
+    train_data = traj_dataset(training_data['obs_traj_rel'], training_data['pred_traj_rel'],training_data['obs_traj'], training_data['pred_traj'])
+    val_data   = traj_dataset(validation_data['obs_traj_rel'], validation_data['pred_traj_rel'],validation_data['obs_traj'], validation_data['pred_traj'])
+    test_data  = traj_dataset(test_data['obs_traj_rel'], test_data['pred_traj_rel'], test_data['obs_traj'], test_data['pred_traj'])
+
+    # Form batches
+    batched_train_data = torch.utils.data.DataLoader(train_data,batch_size=args.batch_size,shuffle=False)
+    batched_val_data   = torch.utils.data.DataLoader(val_data,batch_size=args.batch_size,shuffle=False)
+    batched_test_data  = torch.utils.data.DataLoader(test_data,batch_size=args.batch_size,shuffle=False)
+    # Seleccionamos de forma aleatorea las semillas
+    seeds = np.random.choice(99999999, num_ensembles , replace=False)
+    print("Seeds: ", seeds)
 
 
 
-if args.no_retrain==False:
-    # Entrenamos el modelo para cada semilla
-    for ind, seed in enumerate(seeds):
-        # Agregamos la semilla
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
+    if args.no_retrain==False:
+        # Entrenamos el modelo para cada semilla
+        for ind, seed in enumerate(seeds):
+            # Agregamos la semilla
+            torch.manual_seed(seed)
+            torch.cuda.manual_seed(seed)
 
-        # Instanciate the model
-        model = lstm_encdec(2,128,256,2)
-        model.to(device)
+            # Instanciate the model
+            model = lstm_encdec(2,128,256,2)
+            model.to(device)
 
-        # Entremamos el modelo
-        print("\n*** Training for seed: ", seed, "\t\t ", ind, "/",len(seeds))
-        train(model,ind,idTest)
+            # Entremamos el modelo
+            print("\n*** Training for seed: ", seed, "\t\t ", ind, "/",len(seeds))
+            train(model,device,ind,idTest,batched_train_data,batched_val_data)
 
-        plt.savefig("images/loss_"+str(ind)+"_"+str(idTest)+".pdf")
+            plt.savefig("images/loss_"+str(ind)+"_"+str(idTest)+".pdf")
+            plt.show()
+
+
+    # Instanciamos el modelo
+    model = lstm_encdec(2,128,256,2)
+    model.to(device)
+
+
+    ind_sample = 1
+    bck = plt.imread(os.path.join(dataset_dir,dataset_names[idTest],'reference.png'))
+
+    # Testing
+    for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in enumerate(batched_test_data):
+
+        plt.figure(figsize=(12,12))
+        plt.imshow(bck)
+
+        param_gaussiana = []
+
+        # prediction
+        for ind in range(num_ensembles):
+
+            # Cargamos el Modelo
+            model.load_state_dict(torch.load("training_checkpoints/model_deterministic_"+str(ind)+"_"+str(idTest)+".pth"))
+            model.eval()
+
+            if torch.cuda.is_available():
+                  datarel_test  = datarel_test.to(device)
+
+            pred, sigmas = model.predict(datarel_test, dim_pred=12)
+            print(sigmas)
+            # ploting
+            plot_traj(pred[ind_sample,:,:], data_test[ind_sample,:,:], target_test[ind_sample,:,:], test_homography, bck)
+        plt.legend()
+        plt.title('Trajectory samples')
+        plt.show()
+        # Solo aplicamos a un elemento del batch
+        break
+
+
+    # ## Calibramos la incertidumbre
+    draw_ellipse = True
+
+    # Testing
+    cont = 0
+    for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in enumerate(batched_test_data):
+
+        tpred_samples = []
+        sigmas_samples = []
+        # Muestreamos con cada modelo
+        for ind in range(num_ensembles):
+
+            # Cargamos el Modelo
+            model.load_state_dict(torch.load("training_checkpoints/model_deterministic_"+str(ind)+"_"+str(idTest)+".pth"))
+            model.eval()
+
+            if torch.cuda.is_available():
+                  datarel_test  = datarel_test.to(device)
+
+            pred, sigmas = model.predict(datarel_test, dim_pred=12)
+
+            tpred_samples.append(pred)
+            sigmas_samples.append(sigmas)
+
         plt.show()
 
+        tpred_samples = np.array(tpred_samples)
+        sigmas_samples = np.array(sigmas_samples)
 
-# Instanciamos el modelo
-model = lstm_encdec(2,128,256,2)
-model.to(device)
+        # HDR y Calibracion
+        auc_cal, auc_unc, exp_proportions, obs_proportions_unc, obs_proportions_cal = calibration(tpred_samples, data_test, target_test, sigmas_samples, position = 11, alpha = 0.05, idTest=idTest)
+        plt.show()
 
-
-# In[11]:
-
-
-ind_sample = 1
-bck = plt.imread(os.path.join(dataset_dir,dataset_names[idTest],'reference.png'))
-
-# Testing
-for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in enumerate(batched_test_data):
-
-    plt.figure(figsize=(12,12))
-    plt.imshow(bck)
-
-    param_gaussiana = []
-
-    # prediction
-    for ind in range(num_ensembles):
-
-        # Cargamos el Modelo
-        model.load_state_dict(torch.load("training_checkpoints/model_deterministic_"+str(ind)+"_"+str(idTest)+".pth"))
-        model.eval()
-
-        if torch.cuda.is_available():
-              datarel_test  = datarel_test.to(device)
-
-        pred, sigmas = model.predict(datarel_test, dim_pred=12)
-        print(sigmas)
-        # ploting
-        plot_traj(pred[ind_sample,:,:], data_test[ind_sample,:,:], target_test[ind_sample,:,:], test_homography, bck)
-    plt.legend()
-    plt.title('Trajectory samples')
-    plt.show()
-    # Solo aplicamos a un elemento del batch
-    break
+        # Solo se ejecuta para un batch
+        break
 
 
-# ## Calibramos la incertidumbre
+    # ## Metrics Calibration
 
-# In[12]:
+    ma1    = miscalibration_area(exp_proportions, obs_proportions_unc)
+    mace1  = mean_absolute_calibration_error(exp_proportions, obs_proportions_unc)
+    rmsce1 = root_mean_squared_calibration_error(exp_proportions, obs_proportions_unc)
 
-
-draw_ellipse = True
-
-# Testing
-cont = 0
-for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in enumerate(batched_test_data):
-
-    tpred_samples = []
-    sigmas_samples = []
-    # Muestreamos con cada modelo
-    for ind in range(num_ensembles):
-
-        # Cargamos el Modelo
-        model.load_state_dict(torch.load("training_checkpoints/model_deterministic_"+str(ind)+"_"+str(idTest)+".pth"))
-        model.eval()
-
-        if torch.cuda.is_available():
-              datarel_test  = datarel_test.to(device)
-
-        pred, sigmas = model.predict(datarel_test, dim_pred=12)
-
-        tpred_samples.append(pred)
-        sigmas_samples.append(sigmas)
-
-    plt.show()
-
-    tpred_samples = np.array(tpred_samples)
-    sigmas_samples = np.array(sigmas_samples)
-
-    # HDR y Calibracion
-    auc_cal, auc_unc, exp_proportions, obs_proportions_unc, obs_proportions_cal = calibration(tpred_samples, data_test, target_test, sigmas_samples, position = 11, alpha = 0.05, idTest=idTest)
-    plt.show()
-
-    # Solo se ejecuta para un batch
-    break
+    print("Before Recalibration:  ", end="")
+    print("MACE: {:.5f}, RMSCE: {:.5f}, MA: {:.5f}".format(mace1, rmsce1, ma1))
 
 
-# ## Metrics Calibration
-
-ma1    = miscalibration_area(exp_proportions, obs_proportions_unc)
-mace1  = mean_absolute_calibration_error(exp_proportions, obs_proportions_unc)
-rmsce1 = root_mean_squared_calibration_error(exp_proportions, obs_proportions_unc)
-
-print("Before Recalibration:  ", end="")
-print("MACE: {:.5f}, RMSCE: {:.5f}, MA: {:.5f}".format(mace1, rmsce1, ma1))
+    # In[14]:
 
 
-# In[14]:
+    ma2 = miscalibration_area(exp_proportions, obs_proportions_cal)
+    mace2 = mean_absolute_calibration_error(exp_proportions, obs_proportions_cal)
+    rmsce2 = root_mean_squared_calibration_error(exp_proportions, obs_proportions_cal)
+
+    print("After Recalibration:  ", end="")
+    print("MACE: {:.5f}, RMSCE: {:.5f}, MA: {:.5f}".format(mace2, rmsce2, ma2))
 
 
-ma2 = miscalibration_area(exp_proportions, obs_proportions_cal)
-mace2 = mean_absolute_calibration_error(exp_proportions, obs_proportions_cal)
-rmsce2 = root_mean_squared_calibration_error(exp_proportions, obs_proportions_cal)
-
-print("After Recalibration:  ", end="")
-print("MACE: {:.5f}, RMSCE: {:.5f}, MA: {:.5f}".format(mace2, rmsce2, ma2))
+    df = pd.DataFrame([["","MACE","RMSCE","MA"],["Before Recalibration", mace1, rmsce1, ma1],["After Recalibration", mace2, rmsce2, ma2]])
+    df.to_csv("images/metrics_calibration_"+str(idTest)+".csv")
 
 
-# In[15]:
 
-
-df = pd.DataFrame([["","MACE","RMSCE","MA"],["Before Recalibration", mace1, rmsce1, ma1],["After Recalibration", mace2, rmsce2, ma2]])
-df.to_csv("images/metrics_calibration_"+str(idTest)+".csv")
+if __name__ == "__main__":
+    main()
