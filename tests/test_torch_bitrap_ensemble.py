8c4613f8895e6e43c2ad9a273b4339ff16916127
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-07-26 16:47:20 -0500

    Started integration

--- tests/test_torch_bitrap_ensemble.py
+++ tests/test_torch_bitrap_ensemble.py
@@ -2,8 +2,8 @@ import pdb
 import os
 import sys
 sys.path.append(os.path.realpath('.'))
-sys.path.append('../../bidireaction-trajectory-prediction/')
-sys.path.append('../../bidireaction-trajectory-prediction/datasets')
+sys.path.append('../bidireaction-trajectory-prediction/')
+sys.path.append('../bidireaction-trajectory-prediction/datasets')
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
@@ -14,9 +14,11 @@ from bitrap.modeling import make_model
 from bitrap.engine.trainer import inference
 from bitrap.utils.dataset_utils import restore
 from bitrap.engine.utils import print_info, post_process
-
 from bitrap.utils.logger import Logger
+from utils.datasets_utils import Experiment_Parameters, setup_loo_experiment, traj_dataset
 import logging
+# Local constants
+from utils.constants import OBS_TRAJ_REL, PRED_TRAJ_REL, OBS_TRAJ, PRED_TRAJ, TRAINING_CKPT_DIR, REFERENCE_IMG
 
 import argparse
 from configs import cfg
@@ -30,6 +32,12 @@ import random
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="PyTorch Object Detection Training")
     parser.add_argument('--gpu', default='0', type=str)
+    parser.add_argument('--id-test',
+                        type=int, default=2, metavar='N',
+                        help='id of the dataset to use as test in LOO (default: 2)')
+    parser.add_argument('--batch-size', '--b',
+                        type=int, default=1, metavar='N',
+                        help='input batch size for training (default: 256)')
     parser.add_argument(
         "--config_file",
         default="",
@@ -45,6 +53,26 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
+
+    # Load the default parameters
+    experiment_parameters = Experiment_Parameters(add_kp=False,obstacles=False)
+
+    dataset_dir   = "datasets/"
+    dataset_names = ['eth-hotel','eth-univ','ucy-zara01','ucy-zara02','ucy-univ']
+    model_name = 'model_deterministic'
+
+    # Load the dataset and perform the split
+    training_data, validation_data, test_data, test_homography = setup_loo_experiment('ETH_UCY',dataset_dir,dataset_names,args.id_test,experiment_parameters,pickle_dir='pickle',use_pickled_data=False)
+    # Torch dataset
+    train_data = traj_dataset(training_data[OBS_TRAJ_REL ], training_data[PRED_TRAJ_REL],training_data[OBS_TRAJ], training_data[PRED_TRAJ])
+    val_data = traj_dataset(validation_data[OBS_TRAJ_REL ], validation_data[PRED_TRAJ_REL],validation_data[OBS_TRAJ], validation_data[PRED_TRAJ])
+    test_data = traj_dataset(test_data[OBS_TRAJ_REL ], test_data[PRED_TRAJ_REL], test_data[OBS_TRAJ], test_data[PRED_TRAJ])
+
+    # Form batches
+    batched_train_data = torch.utils.data.DataLoader(train_data,batch_size=args.batch_size,shuffle=False)
+    batched_val_data   = torch.utils.data.DataLoader(val_data,batch_size=args.batch_size,shuffle=False)
+    batched_test_data  = torch.utils.data.DataLoader(test_data,batch_size=args.batch_size,shuffle=False)
+
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
     cfg.BATCH_SIZE = 1
@@ -56,7 +84,7 @@ if __name__ == '__main__':
     nsamples = 100
     ensemble = []
     for i in range(5):
-        cfg.CKPT_DIR = '../training_checkpoints/zara01-0{}.pth'.format(i+1)
+        cfg.CKPT_DIR = 'training_checkpoints/bitrap-zara01-0{}.pth'.format(i+1)
         # build model, optimizer and scheduler
         model = make_model(cfg)
         model = model.to(cfg.DEVICE)
@@ -65,7 +93,7 @@ if __name__ == '__main__':
             print(colored('Loaded checkpoint:{}'.format(cfg.CKPT_DIR), 'blue', 'on_green'))
         else:
             print(colored('The cfg.CKPT_DIR id not a file: {}'.format(cfg.CKPT_DIR), 'green', 'on_red'))
-        #model.K = nsamples
+        model.K = 1
         ensemble.append(model)
 
     # Generate all paths
@@ -98,8 +126,7 @@ if __name__ == '__main__':
                 # transfer back to global coordinates
                 ret = post_process(cfg, X_global, y_global, pred_traj_, pred_goal=pred_goal_, dist_traj=dist_traj_, dist_goal=dist_goal_)
                 X_global_, y_global_, pred_goal_, pred_traj_, dist_traj_, dist_goal_ = ret
-                traj_id = random.randint(0,model.K-1)
-                pred_traj[0,:,k,:] = pred_traj_[0,:,traj_id,:]
+                pred_traj[0,:,k,:] = pred_traj_[0,:,0,:]
 
             key =  list(neighbors_un.keys())[0]
             neighbors = neighbors_un[key][0]
