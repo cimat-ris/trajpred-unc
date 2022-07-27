105fba72ec041f0d1016546234449c63d547d082
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-07-27 17:52:01 -0500

    Modifying the dataset preparation

--- utils/datasets_utils.py
+++ utils/datasets_utils.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 import torch
 from torch.utils.data import Dataset
 from utils.constants import (
-    FRAMES_IDS, KEY_IDX, OBS_NEIGHBORS, OBS_TRAJ, OBS_TRAJ_REL, OBS_TRAJ_THETA, PRED_TRAJ, PRED_TRAJ_REL,
+    FRAMES_IDS, KEY_IDX, OBS_NEIGHBORS, OBS_TRAJ, OBS_TRAJ_VEL, OBS_TRAJ_ACC, OBS_TRAJ_THETA, PRED_TRAJ, PRED_TRAJ_VEL, PRED_TRAJ_ACC,
     TRAIN_DATA_STR, TEST_DATA_STR, VAL_DATA_STR, MUN_POS_CSV
 )
 import logging
@@ -16,22 +16,15 @@ import logging
 # Parameters
 # The only datasets that can use add_kp are PETS2009-S2L1, TOWN-CENTRE
 class Experiment_Parameters:
-    def __init__(self,add_kp=False,obstacles=False):
+    def __init__(self):
         # Maximum number of persons in a frame
         self.person_max =70
         # Observation length (trajlet size)
         self.obs_len    = 8
         # Prediction length
         self.pred_len   = 12
-        # Number of key points
-        self.kp_num     = 18
-        # Key point flag
-        self.add_kp     = add_kp
-        # Obstacles flag
-        self.obstacles    = obstacles
-        self.intersection = False
+        # Delimiter
         self.delim        = ','
-        self.output_representation = 'dxdy' #
 
 # Creamos la clase para el dataset
 class traj_dataset(Dataset):
@@ -87,75 +80,81 @@ def prepare_data(datasets_path, datasets_names, parameters):
     logging.info("Sequence length (observation+prediction): {}".format(seq_len))
 
     # Lists that will hold the data
-    num_person_starting_at_frame = []
     seq_pos_all                  = []
     seq_theta_all                = []
-    seq_rel_all                  = []
+    seq_vel_all                  = []
+    seq_acc_all                  = []
     seq_neighbors_all            = []
     seq_frames_all               = []  # [N, seq_len]
-    all_flow                     = []
-    all_vis_neigh                = []
-    all_vis_obst                 = []
+
     # Scan all the datasets
     for idx,dataset_name in enumerate(datasets_names):
         seq_neighbors_dataset= []
         seq_pos_dataset      = []
         traj_data_path       = os.path.join(datasets_path+dataset_name, MUN_POS_CSV)
         logging.info("Reading "+traj_data_path)
-        obstacles_world = None
 
         # Raw trajectory coordinates
         raw_traj_data = np.genfromtxt(traj_data_path, delimiter= parameters.delim)
 
         # We suppose that the frame ids are in ascending order
         frame_ids = np.unique(raw_traj_data[:, 0]).tolist()
+        ped_ids   = np.unique(raw_traj_data[:, 1]).tolist()
         logging.info("Total number of frames: {}".format(len(frame_ids)))
         logging.info("Total number of agents: {}".format(len(np.unique(raw_traj_data[:, 1]).tolist())))
 
         raw_traj_data_per_frame = [] # people in frame
+        raw_traj_data_per_ped   = {} # dictionnary of full trajectories, indexed by person id
         # Group the spatial pedestrian data frame by frame
         # List indexed by frame ids.
         # Data: id_frame, id_person, x, y
         for frame in frame_ids:
             raw_traj_data_per_frame.append(raw_traj_data[raw_traj_data[:, 0]==frame, :])
+        for ped in ped_ids:
+            t  = raw_traj_data[raw_traj_data[:, 1]==ped,0:1]
+            px = raw_traj_data[raw_traj_data[:, 1]==ped,2]
+            py = raw_traj_data[raw_traj_data[:, 1]==ped,3]
+            if len(px)>1:
+                # TODO: see where to read the dt info (0.4)
+                vx = np.gradient(px,0.4)
+                ax = np.gradient(vx,0.4)
+                vx = np.expand_dims(vx,axis=1)
+                ax = np.expand_dims(ax,axis=1)
+                vy = np.gradient(py,0.4)
+                ay = np.gradient(vy,0.4)
+                vy = np.expand_dims(vy,axis=1)
+                ay = np.expand_dims(ay,axis=1)
+                pv = np.concatenate([t,np.expand_dims(px,axis=1),np.expand_dims(py,axis=1),vx,vy,ax,ay],axis=1)
+                raw_traj_data_per_ped[ped]=pv
         counter = 0
         # Iterate over the frames
         for idx, frame in enumerate(frame_ids):
+            if idx+seq_len>=len(frame_ids):
+                break
+            frame_max      = frame_ids[idx+seq_len]
             # Consider frame sequences of size seq_len = obs+pred
             # id_frame, id_person, x, y por every person present in the frame
-            raw_seq_data = raw_traj_data_per_frame[idx:idx+seq_len]
-            if parameters.intersection:
-                # Intersection of the id_person of "raw_seq_data"
-                # i.e. the pedestrian ids present in ALL th frames
-                peds_in_seq = reduce(set.intersection,
-                                [set(peds_id_list[:,1]) for peds_id_list in
-                                raw_seq_data])
-                peds_in_seq = sorted(list(peds_in_seq))
-                raw_seq_data= np.concatenate(raw_seq_data,axis=0)
-            else:
-                raw_seq_data= np.concatenate(raw_seq_data,axis=0)
-                # Unique indices for the persons in the sequence "raw_seq_data"
-                peds_in_seq = np.unique(raw_seq_data[:,1])
-                # List of all the persons in this sequence
-                peds_in_seq = list(peds_in_seq)
+            raw_seq_data   = raw_traj_data_per_frame[idx:idx+seq_len]
+            raw_seq_data   = np.concatenate(raw_seq_data,axis=0)
+            # Unique indices for the persons in the sequence "raw_seq_data"
+            peds_in_seq    = list(np.unique(raw_seq_data[:,1]))
             # Number of pedestrians to consider
-            num_peds_in_seq = len(peds_in_seq)
+            num_peds_in_seq= len(peds_in_seq)
 
-            # The following two arrays have the same shape
+            # The following arrays have the same shape
             # "pos_seq_data" contains all the absolute positions of all the pedestrians in the sequence
             # and he information is encoded in an absolute frame (no transformation)
-            pos_seq_data = np.zeros((num_peds_in_seq, seq_len, 2), dtype="float32")
-            # Same, with only the displacements
-            rel_seq_data = np.zeros((num_peds_in_seq, seq_len, 2), dtype="float32")
+            pos_seq_data   = np.zeros((num_peds_in_seq, seq_len, 2), dtype="float32")
+            # Same, with only the velocities and accelerations
+            vel_seq_data   = np.zeros((num_peds_in_seq, seq_len, 2), dtype="float32")
+            acc_seq_data   = np.zeros((num_peds_in_seq, seq_len, 2), dtype="float32")
             # Same with orientations
             theta_seq_data = np.zeros((num_peds_in_seq, seq_len, 1), dtype="float32")
             # Is the array that have the sequence of Id_person of all people that there are in frame sequence
             frame_ids_seq_data = np.zeros((num_peds_in_seq, seq_len), dtype="int32")
 
-            # Maximum number of persons in a frame
-            person_max = parameters.person_max
-            # Absolute pixel-based data: id_person, x, y
-            neighbors_data = np.zeros((num_peds_in_seq, seq_len, person_max, 3),dtype="float32")
+            # Tensor for holding nighbor data
+            neighbors_data = np.zeros((num_peds_in_seq, seq_len, parameters.person_max, 3),dtype="float32")
             neighbors_data[:] = np.NaN
 
             ped_count = 0
@@ -163,25 +162,18 @@ def prepare_data(datasets_path, datasets_names, parameters):
             # We will make one entry in the sequences list
             for ped_id in peds_in_seq:
                 # Get the information about ped_id, in the whole sequence
-                ped_seq_data = raw_seq_data[raw_seq_data[:,1]==ped_id,:]
+                ped_seq_data      = raw_seq_data[raw_seq_data[:,1]==ped_id,:]
+                if (not (ped_id in raw_traj_data_per_ped.keys())):
+                    continue
+                ped_seq_data_full = raw_traj_data_per_ped[ped_id]
+                ped_seq_data_mod  = ped_seq_data_full[(ped_seq_data_full[:,0]>=frame) & (ped_seq_data_full[:,0]<frame_max)]
                 # We want pedestrians whose number of observations inside this sequence is exactly seq_len
-                if len(ped_seq_data) != seq_len:
+                if len(ped_seq_data_mod) != seq_len:
                     # We do not have enough observations for this person
                     continue
 
-                # List of all the persons in the frame, to build the neighbors array
-                # Check whether the first 8 positions are not the same
-                # TODO: is it OK to do that?
-                equal_consecutive = 0
-                for n in range(obs_len-1):
-                    if((ped_seq_data[n,2]==ped_seq_data[n+1,2]) and (ped_seq_data[n,3]==ped_seq_data[n+1,3])):
-                        equal_consecutive +=1
-                if(equal_consecutive==obs_len-1):
-                    counter = counter +1
-                    continue
-
                 # To keep neighbors data for the person ped_id
-                neighbors_ped_seq = np.zeros((seq_len, person_max, 3),dtype="float32")
+                neighbors_ped_seq = np.zeros((seq_len, parameters.person_max, 3),dtype="float32")
                 # Scan all the frames of the sequence
                 for frame_idx,frame_id in enumerate(np.unique(raw_seq_data[:,0]).tolist()):
                     # Information of frame "frame_id"
@@ -198,38 +190,27 @@ def prepare_data(datasets_path, datasets_names, parameters):
                 # Contains the neighbor data for ped_count
                 neighbors_data[ped_count,:,:,:] = neighbors_ped_seq
 
-                # Spatial data (absolute positions) for ped
-                ped_seq_pos = ped_seq_data[:,2:]
-                # Spatial data (relative)
-                ped_seq_rel = np.zeros_like(ped_seq_pos)
-                if parameters.output_representation=='dxdy':
-                    # First frame of the relative array is set to zeros
-                    ped_seq_rel[1:, :] = ped_seq_pos[1:, :] - ped_seq_pos[:-1, :]
-                else:
-                    ped_seq_rel[1:, 0] = np.log(0.001+np.linalg.norm(ped_seq_pos[1:, :] - ped_seq_pos[:-1, :],axis=1)/0.5)
-                    ped_seq_rel[1:, 1] = (np.arctan2(ped_seq_pos[1:, 1] - ped_seq_pos[:-1, 1],ped_seq_pos[1:, 0] - ped_seq_pos[:-1, 0]))/10.0
-                    ped_seq_rel[0, :]  = ped_seq_rel[1, :]
-
-                # Absolute x,y and displacements for all person_id
-                pos_seq_data[ped_count, :, :] = ped_seq_pos
-                rel_seq_data[ped_count, :, :] = ped_seq_rel
+                # Absolute x,y and velocities for all person_id
+                pos_seq_data[ped_count, :, :] = ped_seq_data_mod[:,1:3]
+                vel_seq_data[ped_count, :, :] = ped_seq_data_mod[:,3:5]
+                acc_seq_data[ped_count, :, :] = ped_seq_data_mod[:,5:7]
                 # Orientations
-                theta_seq_data[ped_count,1:, 0] = np.arctan2(ped_seq_pos[1:, 1] - ped_seq_pos[:-1, 1],ped_seq_pos[1:, 0] - ped_seq_pos[:-1, 0])
-                theta_seq_data[ped_count,0,  0] = theta_seq_data[ped_count,1,  0]
+                # TODO
+                # theta_seq_data[ped_count,1:, 0] = np.arctan2(ped_seq_pos[1:, 1] - ped_seq_pos[:-1, 1],ped_seq_pos[1:, 0] - ped_seq_pos[:-1, 0])
+                # theta_seq_data[ped_count,0,  0] = theta_seq_data[ped_count,1,  0]
 
                 # For each tracked person
                 # we keep the list of all the frames in which it is present
-                frame_ids_seq_data[ped_count, :] = frame_ids[idx:idx+seq_len]
+                frame_ids_seq_data[ped_count, :] = ped_seq_data_mod[:,0]
                 # Increment ped_count (persons )
                 ped_count += 1
 
-            # Number of persons getting a sequence starting at this frame
-            num_person_starting_at_frame.append(ped_count)
             # Only count_ped data are preserved in the following three arrays
             # Append all the trajectories (pos_seq_data) starting at this frame
             seq_pos_all.append(pos_seq_data[:ped_count])
             # Append all the displacement trajectories (pos_seq_data) starting at this frame
-            seq_rel_all.append(rel_seq_data[:ped_count])
+            seq_vel_all.append(vel_seq_data[:ped_count])
+            seq_acc_all.append(acc_seq_data[:ped_count])
             seq_theta_all.append(theta_seq_data[:ped_count])
             # Append all the frame ranges (frame_ids_seq_data) starting at this frame
             seq_frames_all.append(frame_ids_seq_data[:ped_count])
@@ -243,35 +224,38 @@ def prepare_data(datasets_path, datasets_names, parameters):
         # Neighbors information
         seq_neighbors_dataset = np.concatenate(seq_neighbors_dataset, axis = 0)
         obs_neighbors         = seq_neighbors_dataset[:,:obs_len,:,:]
-        seq_pos_dataset = np.concatenate(seq_pos_dataset,axis=0)
-        obs_traj        = seq_pos_dataset[:, :obs_len, :]
-        logging.info("Total number of trajlets in this dataset: {}".format(obs_traj.shape[0]))
+        seq_pos_dataset       = np.concatenate(seq_pos_dataset,axis=0)
+        logging.info("Total number of trajlets in this dataset: {}".format(seq_pos_dataset.shape[0]))
+
     # Upper level (all datasets)
     # Concatenate all the content of the lists (pos/relative pos/frame ranges)
     seq_pos_all   = np.concatenate(seq_pos_all, axis=0)
-    seq_rel_all   = np.concatenate(seq_rel_all, axis=0)
+    seq_vel_all   = np.concatenate(seq_vel_all, axis=0)
+    seq_acc_all   = np.concatenate(seq_acc_all, axis=0)
     seq_theta_all = np.concatenate(seq_theta_all, axis=0)
     seq_frames_all= np.concatenate(seq_frames_all, axis=0)
     seq_neighbors_all = np.concatenate(seq_neighbors_all, axis=0)
     logging.info("Total number of sample sequences: {}".format(len(seq_pos_all)))
 
-    # We get the obs traj and pred_traj
-    # [total, obs_len, 2]
-    # [total, pred_len, 2]
+    # We split into the obs traj and pred_traj
+    # [total, obs_len, 2] and  [total, pred_len, 2]
     obs_traj      = seq_pos_all[:, :obs_len, :]
     obs_traj_theta= seq_theta_all[:, :obs_len, :]
     pred_traj     = seq_pos_all[:, obs_len:, :]
     frame_obs     = seq_frames_all[:, :obs_len]
-    obs_traj_rel  = seq_rel_all[:, :obs_len, :]
-    pred_traj_rel = seq_rel_all[:, obs_len:, :]
+    obs_traj_vel  = seq_vel_all[:, :obs_len, :]
+    pred_traj_vel = seq_vel_all[:, obs_len:, :]
+    obs_traj_acc  = seq_acc_all[:, :obs_len, :]
+    pred_traj_acc = seq_acc_all[:, obs_len:, :]
     neighbors_obs= seq_neighbors_all[:, :obs_len, :]
     # Save all these data as a dictionary
     data = {
         OBS_TRAJ:       obs_traj,
-        OBS_TRAJ_REL:   obs_traj_rel,
+        OBS_TRAJ_VEL:   obs_traj_vel,
+        OBS_TRAJ_ACC:   obs_traj_acc,
         OBS_TRAJ_THETA: obs_traj_theta,
         PRED_TRAJ:      pred_traj,
-        PRED_TRAJ_REL:  pred_traj_rel,
+        PRED_TRAJ_VEL:  pred_traj_vel,
         FRAMES_IDS:     frame_obs,
         OBS_NEIGHBORS:  neighbors_obs
     }
@@ -310,7 +294,6 @@ def setup_loo_experiment(experiment_name,ds_path,ds_names,leave_id,experiment_pa
             PRED_TRAJ:      train_data[PRED_TRAJ][idx_train],
             PRED_TRAJ_REL:  train_data[PRED_TRAJ_REL][idx_train],
             FRAMES_IDS:     train_data[FRAMES_IDS][idx_train],
-#            "obs_optical_flow":train_data["obs_optical_flow"][idx_train]
         }
         # Test set
         testing_data = {
@@ -320,7 +303,6 @@ def setup_loo_experiment(experiment_name,ds_path,ds_names,leave_id,experiment_pa
             PRED_TRAJ:      test_data[PRED_TRAJ][:],
             PRED_TRAJ_REL:  test_data[PRED_TRAJ_REL][:],
             FRAMES_IDS:     test_data[FRAMES_IDS][:],
-#            "obs_optical_flow": test_data["obs_optical_flow"][:]
         }
         # Validation set
         validation_data ={
@@ -330,7 +312,6 @@ def setup_loo_experiment(experiment_name,ds_path,ds_names,leave_id,experiment_pa
             PRED_TRAJ:      train_data[PRED_TRAJ][idx_val],
             PRED_TRAJ_REL:  train_data[PRED_TRAJ_REL][idx_val],
             FRAMES_IDS:     train_data[FRAMES_IDS][idx_val],
-#            "obs_optical_flow": train_data["obs_optical_flow"][idx_val]
         }
         # Training dataset
         pickle_out = open(pickle_dir+TRAIN_DATA_STR+experiment_name+'.pickle',"wb")
