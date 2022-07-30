21deae7663f56c5150e932852d27f3ad9b0a30f6
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-07-29 20:31:25 -0500

    Working on using own data for btitrap. Some normalizatio problems solved.

--- tests/test_torch_bitrap_ensemble.py
+++ tests/test_torch_bitrap_ensemble.py
@@ -10,15 +10,18 @@ from torch.nn import functional as F
 
 import pickle as pkl
 from datasets import make_dataloader
+from datasets.environment import derivative_of
 from bitrap.modeling import make_model
 from bitrap.engine.trainer import inference
 from bitrap.utils.dataset_utils import restore
 from bitrap.engine.utils import print_info, post_process
 from bitrap.utils.logger import Logger
+
+
 from utils.datasets_utils import Experiment_Parameters, setup_loo_experiment, traj_dataset
 import logging
 # Local constants
-from utils.constants import OBS_TRAJ_REL, PRED_TRAJ_REL, OBS_TRAJ, PRED_TRAJ, TRAINING_CKPT_DIR, REFERENCE_IMG
+from utils.constants import OBS_TRAJ_VEL, PRED_TRAJ_VEL, OBS_TRAJ, PRED_TRAJ, TRAINING_CKPT_DIR, REFERENCE_IMG
 
 import argparse
 from configs import cfg
@@ -36,11 +39,11 @@ if __name__ == '__main__':
                         type=int, default=2, metavar='N',
                         help='id of the dataset to use as test in LOO (default: 2)')
     parser.add_argument('--batch-size', '--b',
-                        type=int, default=1, metavar='N',
+                        type=int, default=64, metavar='N',
                         help='input batch size for training (default: 256)')
     parser.add_argument(
         "--config_file",
-        default="",
+        default="../bidireaction-trajectory-prediction/configs/bitrap_np_ETH.yml",
         metavar="FILE",
         help="path to config file",
         type=str,
@@ -51,28 +54,33 @@ if __name__ == '__main__':
         default=None,
         nargs=argparse.REMAINDER,
     )
+    parser.add_argument('--log-level',type=int, default=20,help='Log level (default: 20)')
+    parser.add_argument('--log-file',default='',help='Log file (default: standard output)')
     args = parser.parse_args()
+    # Loggin format
+    logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
 
 
     # Load the default parameters
-    experiment_parameters = Experiment_Parameters(add_kp=False,obstacles=False)
+    experiment_parameters = Experiment_Parameters()
 
     dataset_dir   = "datasets/"
     dataset_names = ['eth-hotel','eth-univ','ucy-zara01','ucy-zara02','ucy-univ']
     model_name = 'model_deterministic'
 
     # Load the dataset and perform the split
-    training_data, validation_data, test_data, test_homography = setup_loo_experiment('ETH_UCY',dataset_dir,dataset_names,args.id_test,experiment_parameters,pickle_dir='pickle',use_pickled_data=False)
+    training_data, validation_data, testing_data, test_homography = setup_loo_experiment('ETH_UCY',dataset_dir,dataset_names,args.id_test,experiment_parameters,pickle_dir='pickle',use_pickled_data=False)
     # Torch dataset
-    train_data = traj_dataset(training_data[OBS_TRAJ_REL ], training_data[PRED_TRAJ_REL],training_data[OBS_TRAJ], training_data[PRED_TRAJ])
-    val_data = traj_dataset(validation_data[OBS_TRAJ_REL ], validation_data[PRED_TRAJ_REL],validation_data[OBS_TRAJ], validation_data[PRED_TRAJ])
-    test_data = traj_dataset(test_data[OBS_TRAJ_REL ], test_data[PRED_TRAJ_REL], test_data[OBS_TRAJ], test_data[PRED_TRAJ])
-    print(training_data.keys())
+    train_data = traj_dataset(training_data[OBS_TRAJ_VEL ], training_data[PRED_TRAJ_VEL],training_data[OBS_TRAJ], training_data[PRED_TRAJ])
+    val_data = traj_dataset(validation_data[OBS_TRAJ_VEL ], validation_data[PRED_TRAJ_VEL],validation_data[OBS_TRAJ], validation_data[PRED_TRAJ])
+    test_data = traj_dataset(testing_data[OBS_TRAJ_VEL ], testing_data[PRED_TRAJ_VEL], testing_data[OBS_TRAJ], testing_data[PRED_TRAJ])
+    print(testing_data[OBS_TRAJ].shape)
     # Form batches
     batched_train_data = torch.utils.data.DataLoader(train_data,batch_size=args.batch_size,shuffle=False)
     batched_val_data   = torch.utils.data.DataLoader(val_data,batch_size=args.batch_size,shuffle=False)
     batched_test_data  = torch.utils.data.DataLoader(test_data,batch_size=args.batch_size,shuffle=False)
-
+    for (datarel_test, targetrel_test, data_test, target_test) in batched_test_data:
+        print(datarel_test.shape)
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
     cfg.BATCH_SIZE = 1
@@ -81,6 +89,7 @@ if __name__ == '__main__':
     logger = logging.Logger("MPED_RNN")
     # get dataloaders
     test_dataloader = make_dataloader(cfg, 'test')
+    env             = test_dataloader.dataset.dataset.env
     nsamples = 100
     ensemble = []
     for i in range(5):
@@ -96,25 +105,49 @@ if __name__ == '__main__':
         model.K = 1
         ensemble.append(model)
 
+
+    #train_env = dill.load(f, encoding='latin1')
+    #node_type=train_env.NodeType[0]
+    #train_env.attention_radius[(node_type, node_type)] = 3.0 #10.0
+
     # Generate all paths
-    all_img_paths    = []
     all_X_globals    = []
     all_pred_trajs   = []
     all_gt_trajs     = []
-    all_timesteps    = []
 
     with torch.set_grad_enabled(False):
         for iters, batch in enumerate(test_dataloader, start=1):
+
+            print(batch['input_x'].shape)
+            # Input: n_batchesx8x6 (un-normalized)
             X_global     = batch['input_x'].to(cfg.DEVICE)
+            # This is the GT: n_batchesx12x2 (un-normalized)
             y_global     = batch['target_y']
-            img_path     = batch['cur_image_file']
-            resolution   = batch['pred_resolution'].numpy()
+            # Input: n_batchesx8x6 (normalized)
             input_x      = batch['input_x_st'].to(cfg.DEVICE)
+            print(batch['input_x'][0])
+            node_type=env.NodeType[0]
+            state = {'PEDESTRIAN':{'position':['x','y'], 'velocity':['x','y'], 'acceleration':['x','y']}}
+            pred_state = {'PEDESTRIAN':{'position':['x','y']}}
+            _, std = env.get_standardize_params(state['PEDESTRIAN'], node_type)
+            std[0:2] = env.attention_radius[(node_type, node_type)]
+            rel_state      = np.zeros_like(batch['input_x'][:,0])
+            rel_state[:,0:2] = np.array(batch['input_x'])[:,-1, 0:2]
+            rel_state=np.expand_dims(rel_state,axis=1)
+            x_st = env.standardize(batch['input_x'], state[node_type], node_type, mean=rel_state, std=std)
+            #y_st = env.standardize(y, pred_state[node.type], node.type, mean=rel_state[0:2])
+            print(x_st)
+            print(batch['input_x_st'][0])
             neighbors_st = restore(batch['neighbors_x_st'])
             neighbors_un = restore(batch['neighbors_x'])
+            key          =  list(neighbors_st.keys())[0]
+            # Should be a list of x tensors 8x6
+            neighbors    = neighbors_st[key][0]
+            # List of edge values
             adjacency    = restore(batch['neighbors_adjacency'])
-            first_history_indices = batch['first_history_index']
+            first_history_indices = torch.tensor([0],dtype=np.int)
             pred_traj    = np.zeros((1,12,nsamples,2))
+            print('Zou')
             for k in range(nsamples):
                 id = random.randint(0,4)
                 pred_goal_, pred_traj_, _, dist_goal_, dist_traj_ = ensemble[id](input_x,
@@ -140,14 +173,10 @@ if __name__ == '__main__':
                 plt.plot(pred_traj[0,:,sample,0],pred_traj[0,:,sample,1],'green')
             plt.axis('equal')
             plt.show()
-            all_img_paths.extend(img_path)
             all_X_globals.append(X_global)
             all_pred_trajs.append(pred_traj)
             all_gt_trajs.append(y_global)
-            all_timesteps.append(batch['timestep'].numpy())
 
-        # Evaluate
-        all_X_globals = np.concatenate(all_X_globals, axis=0)
+        all_X_globals  = np.concatenate(all_X_globals, axis=0)
         all_pred_trajs = np.concatenate(all_pred_trajs, axis=0)
-        all_gt_trajs = np.concatenate(all_gt_trajs, axis=0)
-        all_timesteps = np.concatenate(all_timesteps, axis=0)
+        all_gt_trajs   = np.concatenate(all_gt_trajs, axis=0)
