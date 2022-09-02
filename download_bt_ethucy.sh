e8f2cb429d9fb2812cddb23bcc5a3981f80bffad
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-09-02 09:25:27 -0500

    Added script to download the ETH/UCY data processed a la Trajectron++

---
+++ download_bt_ethucy.sh
@@ -0,0 +1,3 @@
+# This script downloads the ETH/UCY data processed through trajectron++ protocol 
+mkdir -p datasets/trj++
+gdown https://drive.google.com/drive/folders/1I0ts4NzRr3D8paIMevWrcXuxXUsXiy5Y?usp=sharing -O  datasets/trj++ --folder
