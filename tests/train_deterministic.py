811a416fdffa2de6985078815632b027c68d8fc6
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-12-19 11:19:25 -0600

    adapted the training scripts to the new dataset handling function

--- tests/train_deterministic.py
+++ tests/train_deterministic.py
@@ -16,139 +16,121 @@ import torch.optim as optim
 
 # Local models
 from models.lstm_encdec import lstm_encdec
-from utils.datasets_utils import Experiment_Parameters, setup_loo_experiment, traj_dataset
+from utils.datasets_utils import get_ethucy_dataset
 from utils.plot_utils import plot_traj_img
 from utils.train_utils import train
 
 # Local constants
-from utils.constants import OBS_TRAJ_VEL, PRED_TRAJ_VEL, OBS_TRAJ, PRED_TRAJ, TRAINING_CKPT_DIR, REFERENCE_IMG
+from utils.constants import TRAINING_CKPT_DIR
 
 # Parser arguments
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--batch-size', '--b',
-                    type=int, default=256, metavar='N',
-                    help='input batch size for training (default: 256)')
+					type=int, default=256, metavar='N',
+					help='input batch size for training (default: 256)')
 parser.add_argument('--epochs', '--e',
-                    type=int, default=80, metavar='N',
-                    help='number of epochs to train (default: 100)')
+					type=int, default=80, metavar='N',
+					help='number of epochs to train (default: 100)')
 parser.add_argument('--examples',
-                    type=int, default=1, metavar='N',
-                    help='number of examples to exhibit (default: 1)')
+					type=int, default=1, metavar='N',
+					help='number of examples to exhibit (default: 1)')
 parser.add_argument('--id-test',
-                    type=int, default=2, metavar='N',
-                    help='id of the dataset to use as test in LOO (default: 2)')
+					type=int, default=2, metavar='N',
+					help='id of the dataset to use as test in LOO (default: 2)')
 parser.add_argument('--learning-rate', '--lr',
-                    type=float, default=0.0002, metavar='N',
-                    help='learning rate of optimizer (default: 1E-3)')
-parser.add_argument('--no-retrain',
-                    action='store_true',
-                    help='do not retrain the model')
+					type=float, default=0.0002, metavar='N',
+					help='learning rate of optimizer (default: 1E-3)')
 parser.add_argument('--teacher-forcing',
-                    action='store_true',
-                    help='uses teacher forcing during training')
+					action='store_true',
+					help='uses teacher forcing during training')
+parser.add_argument('--no-retrain',
+					action='store_true',
+					help='do not retrain the model')
 parser.add_argument('--pickle',
-                    action='store_true',
-                    help='use previously made pickle files')
+					action='store_true',
+					help='use previously made pickle files')
 parser.add_argument('--show-plot', default=False,
-                    action='store_true', help='show the test plots')
+					action='store_true', help='show the test plots')
 parser.add_argument('--plot-losses',
-                    action='store_true',
-                    help='plot losses curves after training')
+					action='store_true',
+					help='plot losses curves after training')
 parser.add_argument('--log-level',type=int, default=20,help='Log level (default: 20)')
 parser.add_argument('--log-file',default='',help='Log file (default: standard output)')
 args = parser.parse_args()
 
 def main():
-    # Printing parameters
-    torch.set_printoptions(precision=2)
-    # Loggin format
-    logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
-    # Device
-    if torch.cuda.is_available():
-        logging.info(torch.cuda.get_device_name(torch.cuda.current_device()))
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	# Printing parameters
+	torch.set_printoptions(precision=2)
+	# Loggin format
+	logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
+	# Device
+	if torch.cuda.is_available():
+		logging.info(torch.cuda.get_device_name(torch.cuda.current_device()))
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Load the default parameters
-    experiment_parameters = Experiment_Parameters()
+	batched_train_data,batched_val_data,batched_test_data,homography,reference_image = get_ethucy_dataset(args)
+	model_name    = 'deterministic'
 
-    dataset_dir   = "datasets/"
-    dataset_names = ['eth-hotel','eth-univ','ucy-zara01','ucy-zara02','ucy-univ']
-    model_name = 'model_deterministic'
+	# Seed for RNG
+	seed = 17
+	if args.no_retrain==False:
+		# Choose seed
+		torch.manual_seed(seed)
+		torch.cuda.manual_seed(seed)
+		np.random.seed(seed)
 
-    # Load the dataset and perform the split
-    training_data, validation_data, test_data, test_homography = setup_loo_experiment('ETH_UCY',dataset_dir,dataset_names,args.id_test,experiment_parameters,pickle_dir='pickle',use_pickled_data=args.pickle)
-    # Torch dataset
-    train_data = traj_dataset(training_data[OBS_TRAJ_VEL ], training_data[PRED_TRAJ_VEL],training_data[OBS_TRAJ], training_data[PRED_TRAJ])
-    val_data = traj_dataset(validation_data[OBS_TRAJ_VEL ], validation_data[PRED_TRAJ_VEL],validation_data[OBS_TRAJ], validation_data[PRED_TRAJ])
-    test_data = traj_dataset(test_data[OBS_TRAJ_VEL ], test_data[PRED_TRAJ_VEL], test_data[OBS_TRAJ], test_data[PRED_TRAJ])
+		# Instanciate the model
+		model = lstm_encdec(in_size=2, embedding_dim=128, hidden_dim=128, output_size=2)
+		model.to(device)
 
-    # Form batches
-    batched_train_data = torch.utils.data.DataLoader(train_data,batch_size=args.batch_size,shuffle=False)
-    batched_val_data   = torch.utils.data.DataLoader(val_data,batch_size=args.batch_size,shuffle=False)
-    batched_test_data  = torch.utils.data.DataLoader(test_data,batch_size=args.batch_size,shuffle=True)
+		# Train the model
+		train(model,device,0,batched_train_data,batched_val_data,args,model_name)
 
-    # Seed for RNG
-    seed = 17
-    if args.no_retrain==False:
-        # Choose seed
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-        np.random.seed(seed)
+	# Model instantiation
+	model = lstm_encdec(in_size=2, embedding_dim=128, hidden_dim=128, output_size=2)
+	# Load the previously trained model
+	model_filename = TRAINING_CKPT_DIR+"/"+model_name+"_0"+"_"+str(args.id_test)+".pth"
+	logging.info("Loading {}".format(model_filename))
+	model.load_state_dict(torch.load(model_filename))
+	model.to(device)
+	model.eval()
 
-        # Instanciate the model
-        model = lstm_encdec(in_size=2, embedding_dim=128, hidden_dim=128, output_size=2)
-        model.to(device)
+	ind_sample = 1
 
-        # Train the model
-        train(model,device,0,batched_train_data,batched_val_data,args,model_name)
+	# Testing
+	for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in    enumerate(batched_test_data):
+		if torch.cuda.is_available():
+			datarel_test  = datarel_test.to(device)
 
-    # Model instantiation
-    model = lstm_encdec(in_size=2, embedding_dim=128, hidden_dim=128, output_size=2)
-    # Load the previously trained model
-    model_filename = TRAINING_CKPT_DIR+"/"+model_name+"_0"+"_"+str(args.id_test)+".pth"
-    logging.info("Loading {}".format(model_filename))
-    model.load_state_dict(torch.load(model_filename))
-    model.to(device)
-    model.eval()
+		# Prediction
+		pred = model.predict(datarel_test, dim_pred=12)
 
-    ind_sample = 1
-    bck = plt.imread(os.path.join(dataset_dir,dataset_names[args.id_test],REFERENCE_IMG))
+		# Plotting
+		plt.figure(figsize=(12,12))
+		plt.imshow(reference_image)
+		plot_traj_img(pred[ind_sample,:,:], data_test[ind_sample,:,:], target_test[ind_sample,:,:], homography, reference_image)
+		plt.legend()
+		plt.title('Trajectory samples {}'.format(batch_idx))
+		if args.show_plot:
+			plt.show()
+		# Not display more than args.examples
+		if batch_idx==args.examples-1:
+			break
 
-    # Testing
-    for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in    enumerate(batched_test_data):
-
-        if torch.cuda.is_available():
-            datarel_test  = datarel_test.to(device)
-
-        # prediction
-        pred = model.predict(datarel_test, dim_pred=12)
-
-        # Plotting
-        plt.figure(figsize=(12,12))
-        plt.imshow(bck)
-        plot_traj_img(pred[ind_sample,:,:], data_test[ind_sample,:,:], target_test[ind_sample,:,:], test_homography, bck)
-        plt.legend()
-        plt.title('Trajectory samples {}'.format(batch_idx))
-        if args.show_plot:
-            plt.show()
-        # Not display more than args.examples
-        if batch_idx==args.examples-1:
-            break
-
-    # Testing: Quantitative
-    ade  = 0
-    fde  = 0
-    total= 0
-    for batch_idx, (datavel_test, targetvel_test, data_test, target_test) in    enumerate(batched_test_data):
-        if torch.cuda.is_available():
-            datavel_test  = datavel_test.to(device)
-        total += len(datavel_test)
-        # prediction
-        init_pos  = np.expand_dims(data_test[:,-1,:],axis=1)
-        pred_test = model.predict(datavel_test, dim_pred=12) + init_pos
-        ade    += np.average(np.sqrt(np.square(target_test-pred_test).sum(2)),axis=1).sum()
-        fde    += (np.sqrt(np.square(target_test[:,-1,:]-pred_test[:,-1,:]).sum(1))).sum()
-    logging.info("Test ade : {:.4f} ".format(ade/total))
-    logging.info("Test fde : {:.4f} ".format(fde/total))
+	# Testing: Quantitative
+	ade  = 0
+	fde  = 0
+	total= 0
+	for batch_idx, (datavel_test, targetvel_test, data_test, target_test) in enumerate(batched_test_data):
+		if torch.cuda.is_available():
+			datavel_test  = datavel_test.to(device)
+		total += len(datavel_test)
+		# prediction
+		init_pos  = np.expand_dims(data_test[:,-1,:],axis=1)
+		pred_test = model.predict(datavel_test, dim_pred=12) + init_pos
+		ade    += np.average(np.sqrt(np.square(target_test-pred_test).sum(2)),axis=1).sum()
+		fde    += (np.sqrt(np.square(target_test[:,-1,:]-pred_test[:,-1,:]).sum(1))).sum()
+	logging.info("Test ade : {:.4f} ".format(ade/total))
+	logging.info("Test fde : {:.4f} ".format(fde/total))
 if __name__ == "__main__":
-    main()
+	main()
--- tests/train_deterministic_gaussian.py
+++ tests/train_deterministic_gaussian.py
@@ -20,7 +20,7 @@ import torch.optim as optim
 
 # Local models
 from models.lstm_encdec import lstm_encdec_gaussian
-from utils.datasets_utils import Experiment_Parameters, setup_loo_experiment, traj_dataset
+from utils.datasets_utils import get_ethucy_dataset
 from utils.train_utils import train
 from utils.plot_utils import plot_traj_img, plot_traj_world, plot_cov_world
 from utils.calibration import generate_one_batch_test
@@ -34,153 +34,131 @@ from utils.constants import IMAGES_DIR, OBS_TRAJ_VEL, PRED_TRAJ_VEL, OBS_TRAJ, P
 # Parser arguments
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--batch-size', '--b',
-                    type=int, default=256, metavar='N',
-                    help='input batch size for training (default: 256)')
+					type=int, default=256, metavar='N',
+					help='input batch size for training (default: 256)')
 parser.add_argument('--epochs', '--e',
-                    type=int, default=100, metavar='N',
-                    help='number of epochs to train (default: 200)')
+					type=int, default=100, metavar='N',
+					help='number of epochs to train (default: 200)')
 parser.add_argument('--examples',
-                    type=int, default=1, metavar='N',
-                    help='number of examples to exhibit (default: 1)')
+					type=int, default=1, metavar='N',
+					help='number of examples to exhibit (default: 1)')
 parser.add_argument('--id-test',
-                    type=int, default=2, metavar='N',
-                    help='id of the dataset to use as test in LOO (default: 2)')
+					type=int, default=2, metavar='N',
+					help='id of the dataset to use as test in LOO (default: 2)')
 parser.add_argument('--learning-rate', '--lr',
-                    type=float, default=0.0004, metavar='N',
-                    help='learning rate of optimizer (default: 1E-3)')
-parser.add_argument('--no-retrain',
-                    action='store_true',
-                    help='do not retrain the model')
+					type=float, default=0.0004, metavar='N',
+					help='learning rate of optimizer (default: 1E-3)')
 parser.add_argument('--teacher-forcing',
-                    action='store_true',
-                    help='uses teacher forcing during training')
+					action='store_true',
+					help='uses teacher forcing during training')
+parser.add_argument('--no-retrain',
+					action='store_true',
+					help='do not retrain the model')
 parser.add_argument('--pickle',
-                    action='store_true',
-                    help='use previously made pickle files')
+					action='store_true',
+					help='use previously made pickle files')
 parser.add_argument('--show-plot', default=False,
-                    action='store_true', help='show the test plots')
+					action='store_true', help='show the test plots')
 parser.add_argument('--plot-losses',
-                    action='store_true',
-                    help='plot losses curves after training')
+					action='store_true',
+					help='plot losses curves after training')
 parser.add_argument('--log-level',type=int, default=20,help='Log level (default: 20)')
 parser.add_argument('--log-file',default='',help='Log file (default: standard output)')
 args = parser.parse_args()
 
 
 def main():
-    # Printing parameters
-    torch.set_printoptions(precision=2)
-    # Loggin format
-    logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
-    # Device
-    if torch.cuda.is_available():
-        logging.info(torch.cuda.get_device_name(torch.cuda.current_device()))
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	# Printing parameters
+	torch.set_printoptions(precision=2)
+	# Loggin format
+	logging.basicConfig(format='%(levelname)s: %(message)s',level=args.log_level)
+	# Device
+	if torch.cuda.is_available():
+		logging.info(torch.cuda.get_device_name(torch.cuda.current_device()))
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Load the default parameters
-    experiment_parameters = Experiment_Parameters()
+	batched_train_data,batched_val_data,batched_test_data,homography,reference_image = get_ethucy_dataset(args)
+	model_name    = "deterministic_variances"
 
-    dataset_dir   = "datasets/"
-    dataset_names = ['eth-hotel','eth-univ','ucy-zara01','ucy-zara02','ucy-univ']
-    model_name    = "deterministic_variances"
+	# Seed for RNG
+	seed = 1
+	num_ensembles = 1
 
-    # Load the dataset and perform the split
-    training_data, validation_data, test_data, test_homography = setup_loo_experiment('ETH_UCY',dataset_dir,dataset_names,args.id_test,experiment_parameters,pickle_dir='pickle',use_pickled_data=args.pickle)
-    # Torch dataset
-    train_data = traj_dataset(training_data[OBS_TRAJ_VEL], training_data[PRED_TRAJ_VEL],training_data[OBS_TRAJ], training_data[PRED_TRAJ])
-    val_data   = traj_dataset(validation_data[OBS_TRAJ_VEL], validation_data[PRED_TRAJ_VEL],validation_data[OBS_TRAJ], validation_data[PRED_TRAJ])
-    test_data  = traj_dataset(test_data[OBS_TRAJ_VEL], test_data[PRED_TRAJ_VEL], test_data[OBS_TRAJ], test_data[PRED_TRAJ])
+	if args.no_retrain==False:
+		# Choose seed
+		torch.manual_seed(seed)
+		torch.cuda.manual_seed(seed)
 
-    # Form batches
-    batched_train_data = torch.utils.data.DataLoader(train_data,batch_size=args.batch_size,shuffle=False)
-    batched_val_data   = torch.utils.data.DataLoader(val_data,batch_size=args.batch_size,shuffle=False)
-    batched_test_data  = torch.utils.data.DataLoader(test_data,batch_size=args.batch_size,shuffle=False)
+		# Instanciate the model
+		model = lstm_encdec_gaussian(in_size=2, embedding_dim=128, hidden_dim=256, output_size=2)
+		model.to(device)
 
-    # Seed for RNG
-    seed = 1
-    num_ensembles = 1
+		# Train the model
+		train(model,device,0,batched_train_data,batched_val_data,args,model_name)
 
-    if args.no_retrain==False:
-        # Choose seed
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-
-        # Instanciate the model
-        model = lstm_encdec_gaussian(in_size=2, embedding_dim=128, hidden_dim=256, output_size=2)
-        model.to(device)
-
-        # Train the model
-        train(model,device,0,batched_train_data,batched_val_data,args,model_name)
-
-    # Model instantiation
-    model = lstm_encdec_gaussian(in_size=2, embedding_dim=128, hidden_dim=256, output_size=2)
-    # Load the previously trained model
-    model.load_state_dict(torch.load(TRAINING_CKPT_DIR+"/"+model_name+"_0"+"_"+str(args.id_test)+".pth"))
-    model.eval()
-    model.to(device)
+	# Model instantiation
+	model = lstm_encdec_gaussian(in_size=2, embedding_dim=128, hidden_dim=256, output_size=2)
+	# Load the previously trained model
+	model.load_state_dict(torch.load(TRAINING_CKPT_DIR+"/"+model_name+"_0"+"_"+str(args.id_test)+".pth"))
+	model.eval()
+	model.to(device)
 
 
-    ind_sample = np.random.randint(args.batch_size)
-    bck = plt.imread(os.path.join(dataset_dir,dataset_names[args.id_test], REFERENCE_IMG))
+	ind_sample = np.random.randint(args.batch_size)
+	output_dir = os.path.join(IMAGES_DIR)
+	mkdir_p(output_dir)
 
-    output_dir = os.path.join(IMAGES_DIR)
-    mkdir_p(output_dir)
+	# Testing
+	for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in enumerate(batched_test_data):
+		fig, ax = plt.subplots(1,1,figsize=(12,12))
 
-    # Testing
-    for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in enumerate(batched_test_data):
-        fig, ax = plt.subplots(1,1,figsize=(12,12))
+		if torch.cuda.is_available():
+			datarel_test  = datarel_test.to(device)
 
-        if torch.cuda.is_available():
-            datarel_test  = datarel_test.to(device)
+		pred, sigmas = model.predict(datarel_test, dim_pred=12)
+		# Plotting
+		ind = np.minimum(ind_sample,pred.shape[0]-1)
+		plot_traj_world(pred[ind,:,:],data_test[ind,:,:],target_test[ind,:,:],ax)
+		plt.legend()
+		plt.savefig(os.path.join(output_dir , "pred_dropout"+".pdf"))
+		if args.show_plot:
+			plt.show()
+		plt.close()
+		# Not display more than args.examples
+		if batch_idx==args.examples-1:
+			break
 
-        pred, sigmas = model.predict(datarel_test, dim_pred=12)
-        # Plotting
-        ind = np.minimum(ind_sample,pred.shape[0]-1)
-        plot_traj_world(pred[ind,:,:],data_test[ind,:,:],target_test[ind,:,:],ax)
-        #plot_cov_world(pred[ind,:,:],sigmas[ind,:,:],data_test[ind,:,:],ax)
-        plt.legend()
-        plt.savefig(os.path.join(output_dir , "pred_dropout"+".pdf"))
-        if args.show_plot:
-            plt.show()
-        plt.close()
-        # Not display more than args.examples
-        if batch_idx==args.examples-1:
-            break
+	#------------------ Obtenemos el batch unico de test para las curvas de calibracion ---------------------------
+	datarel_test_full, targetrel_test_full, data_test_full, target_test_full, tpred_samples_full, sigmas_samples_full = generate_one_batch_test(batched_test_data, model, num_ensembles, TRAINING_CKPT_DIR, model_name, id_test=args.id_test, device=device)
+	#---------------------------------------------------------------------------------------------------------------
 
-    #------------------ Obtenemos el batch unico de test para las curvas de calibracion ---------------------------
-    datarel_test_full, targetrel_test_full, data_test_full, target_test_full, tpred_samples_full, sigmas_samples_full = generate_one_batch_test(batched_test_data, model, num_ensembles, TRAINING_CKPT_DIR, model_name, id_test=args.id_test, device=device)
-    #---------------------------------------------------------------------------------------------------------------
+	# Testing
+	for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in enumerate(batched_test_data):
 
-    # Testing
-    cont = 0
-    for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in enumerate(batched_test_data):
+		tpred_samples = []
+		sigmas_samples = []
+		# Muestreamos con cada modelo
+		for ind in range(num_ensembles):
 
-        tpred_samples = []
-        sigmas_samples = []
-        # Muestreamos con cada modelo
-        for ind in range(num_ensembles):
+			# Load the model
+			model.load_state_dict(torch.load(TRAINING_CKPT_DIR+"/"+model_name+"_"+str(ind)+"_"+str(args.id_test)+".pth"))
+			model.eval()
 
-            # Cargamos el Modelo
-            model.load_state_dict(torch.load(TRAINING_CKPT_DIR+"/"+model_name+"_"+str(ind)+"_"+str(args.id_test)+".pth"))
-            model.eval()
+			if torch.cuda.is_available():
+				  datarel_test  = datarel_test.to(device)
 
-            if torch.cuda.is_available():
-                  datarel_test  = datarel_test.to(device)
+			pred, sigmas = model.predict(datarel_test, dim_pred=12)
 
-            pred, sigmas = model.predict(datarel_test, dim_pred=12)
+			tpred_samples.append(pred)
+			sigmas_samples.append(sigmas)
 
-            tpred_samples.append(pred)
-            sigmas_samples.append(sigmas)
-
-        tpred_samples = np.array(tpred_samples)
-        sigmas_samples = np.array(sigmas_samples)
-        print(tpred_samples.shape)
-        print(sigmas_samples.shape)
-
-        save_data_for_calibration(DETERMINISTIC_GAUSSIAN, tpred_samples, tpred_samples_full, data_test, data_test_full, target_test, target_test_full, targetrel_test, targetrel_test_full, sigmas_samples, sigmas_samples_full, args.id_test)
-        # Solo se ejecuta para un batch y es usado como dataset de calibración
-        break
+		tpred_samples = np.array(tpred_samples)
+		sigmas_samples = np.array(sigmas_samples)
+		# Save these testing data for uncertainty calibration
+		save_data_for_calibration(DETERMINISTIC_GAUSSIAN, tpred_samples, tpred_samples_full, data_test, data_test_full, target_test, target_test_full, targetrel_test, targetrel_test_full, sigmas_samples, sigmas_samples_full, args.id_test)
+		# Only the first batch is used as the calibration dataset
+		break
 
 
 if __name__ == "__main__":
-    main()
+	main()
