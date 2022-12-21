9e2af88fbd91551bee300f4e3d8c366d11ba311c
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-12-21 17:57:12 -0600

    Slight change

--- train_bitrap_ensembles.sh
+++ train_bitrap_ensembles.sh
@@ -1,10 +1,10 @@
  #!/bin/bash
 for idTest in 'bitrap_np_hotel.yml' 'bitrap_np_eth.yml' 'bitrap_np_zara1.yml' 'bitrap_np_zara2.yml' 'bitrap_np_univ.yml'
 do
-		echo $idTest
+		echo "Dataset: $idTest"
 		for ensembleId in 1 2 3 4 5
 		do
-			echo $ensembleId
+			echo "Ensemble id: $ensembleId"
 			python tests/train_bitrap.py --seed $ensembleId --config_file $idTest
 		done
 done
