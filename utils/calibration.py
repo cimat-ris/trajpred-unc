91f3c6bd6a19b597bfb4aab727e2f52851e63017
Jean Bernard Hayet <jbhayet@cimat.mx>
2023-02-04 19:49:18 -0600

    Slight changes

--- utils/calibration.py
+++ utils/calibration.py
@@ -232,11 +232,10 @@ def evaluate_kde(prediction, sigmas_prediction, ground_truth, kde_size=1000, res
 		# In this case, we just have samples and create the KDE from them
 		f_density = gaussian_kde(prediction.T)
 		# Then we sample from the obtained representation
-		samples = f_density.resample(resample_size,0)
-		f_samples      = f_density.pdf(samples)
+		samples   = f_density.resample(resample_size,0)
+		f_samples = f_density.pdf(samples)
 	# Evaluate the GT and the samples on the obtained KDE
 	f_ground_truth = f_density.pdf(ground_truth.T)
-	f_samples      = f_density.pdf(samples.T)
 	return f_density, f_ground_truth, f_samples, samples
 
 def regresion_isotonic_fit(this_pred_out_abs, data_gt, position, kde_size=1000, resample_size=100, sigmas_prediction=None):
