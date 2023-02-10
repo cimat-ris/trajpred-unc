63189791c5aee056be8299c0598dd52ef95c2674
Jean Bernard Hayet <jbhayet@cimat.mx>
2023-02-10 12:57:38 -0600

    Improving script for agentformer

--- scripts/test_agentformer.py
+++ scripts/test_agentformer.py
@@ -22,47 +22,48 @@ def get_model_prediction(data, sample_k):
 	print(data.keys())
 	print(data['frame'])
 	print(data['valid_id'])
-	recon_motion_3D, _ = model.inference(mode='recon', sample_num=sample_k)
+	recon_motion_3D, _     = model.inference(mode='recon', sample_num=sample_k)
 	sample_motion_3D, data = model.inference(mode='infer', sample_num=sample_k, need_weights=False)
 	# Output future samples: sample_motion_3D: sample_kxnx12x2
 	sample_motion_3D = sample_motion_3D.transpose(0, 1).contiguous()
 	return recon_motion_3D, sample_motion_3D
 
-def save_prediction(pred, data, suffix, save_dir):
+def get_trajectories(predictions, raw_data):
 	pred_num = 0
-	pred_arr = []
-	fut_data, seq_name, frame, valid_id, pred_mask = data['fut_data'], data['seq'], data['frame'], data['valid_id'], data['pred_mask']
+	fut_data, seq_name, frame, valid_id, pred_mask = raw_data['fut_data'], raw_data['seq'], raw_data['frame'], raw_data['valid_id'], raw_data['pred_mask']
+	pred_samples = []
+	for s in range(predictions.shape[0]): # Cycle over the samples
+		pred_arr = []
+		for i in range(len(valid_id)):    # Cycle over the agents
+			identity = valid_id[i]
+			if pred_mask is not None and pred_mask[i] != 1.0:
+				continue
 
-	for i in range(len(valid_id)):    # number of agents
-		identity = valid_id[i]
-		if pred_mask is not None and pred_mask[i] != 1.0:
-			continue
+			"""Cycle over future frames"""
+			trajectory = []
+			for j in range(cfg.future_frames):
+				cur_data = fut_data[j]
+				if len(cur_data) > 0 and identity in cur_data[:, 1]:
+					frame_data = cur_data[cur_data[:, 1] == identity].squeeze()
+				else:
+					frame_data = most_recent_data.copy()
+					frame_data[0] = frame + j + 1
+				frame_data[[13, 15]] = predictions[s, i, j].cpu().numpy()   # [13, 15] corresponds to 2D pos
+				most_recent_data = frame_data.copy()
+				trajectory.append(frame_data)
+				#pred_arr.append(all_data)
+			trajectory = np.array(trajectory)
+			pred_arr.append(trajectory)
+		if len(pred_arr) > 0:
+			pred_arr = np.array(pred_arr)
+			indices = [0, 1, 13, 15]            # frame, ID, x, z (remove y which is the height)
+			pred_arr = pred_arr[:,:,indices]
+			pred_samples.append(pred_arr)
+	return np.array(pred_samples)
 
-		"""future frames"""
-		for j in range(cfg.future_frames):
-			cur_data = fut_data[j]
-			if len(cur_data) > 0 and identity in cur_data[:, 1]:
-				data = cur_data[cur_data[:, 1] == identity].squeeze()
-			else:
-				data = most_recent_data.copy()
-				data[0] = frame + j + 1
-			data[[13, 15]] = pred[i, j].cpu().numpy()   # [13, 15] corresponds to 2D pos
-			most_recent_data = data.copy()
-			pred_arr.append(data)
-		pred_num += 1
-
-	if len(pred_arr) > 0:
-		pred_arr = np.vstack(pred_arr)
-		indices = [0, 1, 13, 15]            # frame, ID, x, z (remove y which is the height)
-		pred_arr = pred_arr[:, indices]
-		# save results
-		fname = f'{save_dir}/{seq_name}/frame_{int(frame):06d}{suffix}.txt'
-		mkdir_if_missing(fname)
-		np.savetxt(fname, pred_arr, fmt="%.3f")
-	return pred_num
-
-def test_model(generator, save_dir, cfg):
+def test_model(generator, cfg):
 	total_num_pred = 0
+	all_predictions= []
 	while not generator.is_epoch_end():
 		data = generator()
 		if data is None:
@@ -77,24 +78,14 @@ def test_model(generator, save_dir, cfg):
 			recon_motion_3D, sample_motion_3D = get_model_prediction(data, cfg.sample_k)
 		recon_motion_3D, sample_motion_3D = recon_motion_3D * cfg.traj_scale, sample_motion_3D * cfg.traj_scale
 
-		"""save samples"""
-		recon_dir = os.path.join(save_dir, 'recon'); mkdir_if_missing(recon_dir)
-		sample_dir = os.path.join(save_dir, 'samples'); mkdir_if_missing(sample_dir)
-		gt_dir = os.path.join(save_dir, 'gt'); mkdir_if_missing(gt_dir)
-		for i in range(sample_motion_3D.shape[0]):
-			save_prediction(sample_motion_3D[i], data, f'/sample_{i:03d}', sample_dir)
-		save_prediction(recon_motion_3D, data, '', recon_dir)        # save recon
-		num_pred = save_prediction(gt_motion_3D, data, '', gt_dir)              # save gt
-		total_num_pred += num_pred
+		# Transform the output into our format for trajectories
+		all_predictions.append(get_trajectories(sample_motion_3D,data))
 
-	print_log(f'\n\n total_num_pred: {total_num_pred}', log)
-	if cfg.dataset == 'nuscenes_pred':
-		scene_num = {
-			'train': 32186,
-			'val': 8560,
-			'test': 9041
-		}
-		assert total_num_pred == scene_num[generator.split]
+	all_predictions = np.concatenate(all_predictions,axis=1)
+	total_num_pred  = all_predictions.shape[1]
+	logging.info(f'\n\n Number of predicted trajectories: {total_num_pred}')
+	return all_predictions
+	# save_data_for_calibration(pickle_filename, tpred_samples, tpred_samples_full, data_test, data_test_full, target_test, target_test_full, targetrel_test, targetrel_test_full, sigmas_samples, sigmas_samples_full, config.id_test)
 
 if __name__ == '__main__':
 	# Loggin format
@@ -104,47 +95,28 @@ if __name__ == '__main__':
 	if torch.cuda.is_available():
 		logging.info(torch.cuda.get_device_name(torch.cuda.current_device()))
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-	# Get the data
-	batched_train_data,batched_val_data,batched_test_data,homography,reference_image = get_dataset(config)
 
 	""" setup """
-	cfg = Config(config.cfg)
-	if config.epochs is None:
-		print(cfg)
-		epochs = [cfg.get_last_epoch()]
-		print(epochs)
-	else:
-		epochs = [int(x) for x in config.epochs.split(',')]
+	cfg   = Config(config.cfg)
+	epoch = cfg.get_last_epoch()
 	torch.set_default_dtype(torch.float32)
 	torch.set_grad_enabled(False)
 	log = open(os.path.join(cfg.log_dir, 'log_test.txt'), 'w')
 
-	for epoch in epochs:
-		prepare_seed(cfg.seed)
-		""" model """
-		if not config.cached:
-			model_id = cfg.get('model_id', 'agentformer')
-			model = model_dict[model_id](cfg)
-			model.set_device(device)
-			model.eval()
-			if epoch > 0:
-				cp_path = cfg.model_path % epoch
-				logging.info(f'loading model from checkpoint: {cp_path}')
-				model_cp = torch.load(cp_path, map_location='cpu')
-				model.load_state_dict(model_cp['model_dict'], strict=False)
+	prepare_seed(cfg.seed)
+	""" Load model """
+	model_id = cfg.get('model_id', 'agentformer')
+	model = model_dict[model_id](cfg)
+	model.set_device(device)
+	model.eval()
+	if epoch > 0:
+		cp_path = cfg.model_path % epoch
+		logging.info(f'Loading model from checkpoint: {cp_path}')
+		model_cp = torch.load(cp_path, map_location='cpu')
+		model.load_state_dict(model_cp['model_dict'], strict=False)
 
-		""" save results and compute metrics """
-		data_splits = [config.data_eval]
-
-		for split in data_splits:
-			generator = data_generator(cfg, log, split=split, phase='testing')
-			save_dir = f'{cfg.result_dir}/epoch_{epoch:04d}/{split}'; mkdir_if_missing(save_dir)
-			eval_dir = f'{save_dir}/samples'
-			if not config.cached:
-				test_model(generator, save_dir, cfg)
-
-			# remove eval folder to save disk space
-			if config.cleanup:
-				shutil.rmtree(save_dir)
+	""" Save results and compute metrics """
+	generator = data_generator(cfg, log, split='test', phase='testing')
+	test_model(generator, cfg)
 
 	# TODO: how to use the same batch for calibration?
