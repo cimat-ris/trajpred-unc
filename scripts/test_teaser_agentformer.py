41232d8ca28069fee9b9f7809b80ae913c90f05d
Jean Bernard Hayet <jbhayet@cimat.mx>
2023-04-12 14:46:48 -0600

    Script for producing the AF result of Table 1

--- scripts/test_teaser_agentformer.py
+++ scripts/test_teaser_agentformer.py
@@ -16,7 +16,9 @@ from utils.datasets_utils import get_dataset
 from utils.calibration_utils import save_data_for_calibration
 from utils.config import get_config
 from utils.constants import SUBDATASETS_NAMES,AGENTFORMER
+from utils.calibration import generate_uncertainty_evaluation_dataset,regression_isotonic_fit,calibrate_and_test
 import scipy.stats as st
+from utils.hdr import get_alpha,get_falpha,sort_sample,samples_to_alphas
 
 configurations = ['hotel_agentformer_pre','eth_agentformer_pre','zara1_agentformer_pre','zara2_agentformer_pre','univ_agentformer_pre']
 
@@ -107,62 +109,101 @@ if __name__ == '__main__':
 	epoch = cfg.get_last_epoch()
 	torch.set_default_dtype(torch.float32)
 	torch.set_grad_enabled(False)
-	log = open(os.path.join(cfg.log_dir, 'log_test.txt'), 'w')
+	log      = open(os.path.join(cfg.log_dir, 'log_test.txt'), 'w')
+	allmfdes_before = []
+	allmfdes_after  = []
+	for seed in range(1,11):
+		"""  Set seed """
+		logging.info("Seed: {}".format(seed))
+		prepare_seed(seed)
+		torch.manual_seed(seed)
+		torch.cuda.manual_seed(seed)
+		random.seed(seed)
+		np.random.seed(seed)
 
-	"""  Set seed """
-	logging.info("Seed: {}".format(config.seed))
-	prepare_seed(cfg.seed)
-	torch.manual_seed(config.seed)
-	torch.cuda.manual_seed(config.seed)
-	random.seed(config.seed)
-	np.random.seed(config.seed)
+		""" Load model """
+		model_id = cfg.get('model_id', 'agentformer')
+		model = model_dict[model_id](cfg)
+		if epoch is None:
+			logging.error("Could not identify model")
+			sys.exit()
+		model.set_device(device)
+		model.eval()
+		if epoch > 0:
+			cp_path = cfg.model_path % epoch
+			logging.info(f'Loading model from checkpoint: {cp_path}')
+			model_cp = torch.load(cp_path, map_location='cpu')
+			model.load_state_dict(model_cp['model_dict'], strict=False)
 
-	""" Load model """
-	model_id = cfg.get('model_id', 'agentformer')
-	model = model_dict[model_id](cfg)
-	if epoch is None:
-		logging.error("Could not identify model")
-		sys.exit()
-	model.set_device(device)
-	model.eval()
-	if epoch > 0:
-		cp_path = cfg.model_path % epoch
-		logging.info(f'Loading model from checkpoint: {cp_path}')
-		model_cp = torch.load(cp_path, map_location='cpu')
-		model.load_state_dict(model_cp['model_dict'], strict=False)
+		""" Apply model on test data """
+		generator = data_generator(cfg, log, split='test', phase='testing')
+		tpred_samples,gt_trajectories,obs_trajectories = apply_model(generator, cfg)
 
-	""" Apply model on test data """
-	generator = data_generator(cfg, log, split='test', phase='testing')
-	tpred_samples,gt_trajectories,obs_trajectories = apply_model(generator, cfg)
+		""" Select sub-dataset of 256 trajectories for uncertainty calibration """
+		select = np.random.permutation(gt_trajectories.shape[0])
+		tpred_samples   = tpred_samples[:,select,:,:]
+		gt_trajectories = gt_trajectories[select,:,:]
+		obs_trajectories= obs_trajectories[select,:,:]
+		tpred_samples_cal =tpred_samples[:,:256,:,:]
+		tpred_samples_test=tpred_samples[:,256:,:,:]
+		data_cal          = obs_trajectories[:256,:,:]
+		data_test         = obs_trajectories[256:,:,:]
+		target_cal        = gt_trajectories[:256,:,:]
+		target_test       = gt_trajectories[256:,:,:]
+		pickle_filename = model_name+"_"+str(SUBDATASETS_NAMES[0][config.id_test])
+		save_data_for_calibration(pickle_filename, tpred_samples_cal, tpred_samples_test, data_cal, data_test, target_cal, target_test, None, None, config.id_test)
 
-	""" Select sub-dataset of 256 trajectories for uncertainty calibration """
-	select = np.random.permutation(gt_trajectories.shape[0])
-	tpred_samples   = tpred_samples[:,select,:,:]
-	gt_trajectories = gt_trajectories[select,:,:]
-	obs_trajectories= obs_trajectories[select,:,:]
-	tpred_samples_cal =tpred_samples[:,:256,:,:]
-	tpred_samples_test=tpred_samples[:,256:,:,:]
-	data_cal          = obs_trajectories[:256,:,:]
-	data_test         = obs_trajectories[256:,:,:]
-	target_cal        = gt_trajectories[:256,:,:]
-	target_test       = gt_trajectories[256:,:,:]
-	pickle_filename = model_name+"_"+str(SUBDATASETS_NAMES[0][config.id_test])
-	save_data_for_calibration(pickle_filename, tpred_samples_cal, tpred_samples_test, data_cal, data_test, target_cal, target_test, None, None, config.id_test)
+		"""  Evaluate minFDE before calibration."""
+		minfdes = []
+		for k in tqdm.tqdm(range(tpred_samples.shape[1])):
+			xs = []
+			ys = []
+			tpred = tpred_samples[:,k,11,:].reshape((-1,2,1))
+			gt    = gt_trajectories[k,11,:].cpu().numpy().reshape((2,1))
+			# Prediction samples
+			world_samples   = tpred_samples[:,k,11,:].reshape((-1,2))
+			if world_samples.shape[0]>0:
+				# Build a Kernel Density Estimator with these samples
+				kde         = st.gaussian_kde(world_samples.T)
+				sub_samples = kde.resample(20).T.reshape((-1,2,1))
+				fdes        = np.sqrt(np.sum(np.square(sub_samples - gt),axis=1))
+				minfdes.append(np.amin(fdes))
+		allmfdes_before.append(np.mean(minfdes))
+		logging.info("minFDE: {:.4f}".format(np.mean(minfdes)))
 
-	"""  Evaluate minFDE before calibration."""
-	minfdes = []
-	for k in tqdm.tqdm(range(tpred_samples.shape[1])):
-		xs = []
-		ys = []
-		tpred = tpred_samples[:,k,11,:].reshape((-1,2,1))
-		gt    = gt_trajectories[k,11,:].cpu().numpy().reshape((2,1))
-		# Prediction samples
-		world_samples   = tpred_samples[:,k,11,:].reshape((-1,2))
-		if world_samples.shape[0]>0:
-			# Build a Kernel Density Estimator with these samples
-			kde         = st.gaussian_kde(world_samples.T)
-			sub_samples = kde.resample(20).T.reshape((-1,2,1))
-			fdes        = np.sqrt(np.sum(np.square(sub_samples - gt),axis=1))
-			minfdes.append(np.amin(fdes))
+		""" Calibration."""
+		# Uncertainty calibration
+		logging.info("Calibration at position: {}".format(11))
+		# Isotonic regression: Gives a mapping from predicted alpha to corrected alpha
+		iso_reg, iso_inv = regression_isotonic_fit(tpred_samples_cal[:,:,11,:],target_cal,11,kde_size=1000,resample_size=100,sigmas_prediction=None)
 
-	logging.info("minFDE: {}".format(np.mean(minfdes)))
+		"""  Evaluate minFDE after calibration."""
+		minfdes = []
+		for k in tqdm.tqdm(range(tpred_samples.shape[1])):
+			xs = []
+			ys = []
+			tpred = tpred_samples[:,k,11,:].reshape((-1,2,1))
+			gt    = gt_trajectories[k,11,:].cpu().numpy().reshape((2,1))
+			# Prediction samples
+			world_samples   = tpred_samples[:,k,11,:].reshape((-1,2))
+			if world_samples.shape[0]>0:
+				# Build a Kernel Density Estimator with these samples
+				kde         = st.gaussian_kde(world_samples.T)
+				# Evaluate our samples on it
+				alphas_samples, fs_samples, sorted_samples = samples_to_alphas(kde,world_samples.T)
+				modified_alphas = iso_inv.transform(alphas_samples)
+				fs_samples_new  = []
+				for alpha in modified_alphas:
+					fs_samples_new.append(get_falpha(sorted_samples,alpha))
+				fs_samples_new    = np.array(fs_samples_new)
+				importance_weights= fs_samples_new/fs_samples
+				kde               = st.gaussian_kde(world_samples.T,weights=importance_weights)
+				sub_samples = kde.resample(20).T.reshape((-1,2,1))
+				fdes        = np.sqrt(np.sum(np.square(sub_samples - gt),axis=1))
+				minfdes.append(np.amin(fdes))
+		allmfdes_after.append(np.mean(minfdes))
+		logging.info("minFDE: {:.4f}".format(np.mean(minfdes)))
+
+
+	logging.info("averaged minFDE before: {:.4f}".format(np.mean(allmfdes_before)))
+	logging.info("averaged minFDE after: {:.4f}".format(np.mean(allmfdes_after)))
