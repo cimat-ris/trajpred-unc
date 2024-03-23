34c0ea15c4ca5226fdf065e6bc71ea4de7076b5b
Jean-Bernard Hayet <jbhayet@cimat.mx>
2024-03-22 20:24:13 -0600

    New version for  the train script for deterministic model

--- scripts/train_deterministic.py
+++ scripts/train_deterministic.py
@@ -6,7 +6,7 @@ import time, random
 import sys,os,logging, argparse
 sys.path.append('.')
 
-import math,numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 
@@ -18,45 +18,41 @@ from models.lstm_encdec import lstm_encdec
 from utils.datasets_utils import get_dataset
 from utils.plot_utils import plot_traj_img
 from utils.train_utils import train
-from utils.config import get_config
+from utils.config import load_config
+from utils.constants import SUBDATASETS_NAMES
 
-# Local constants
-from utils.constants import TRAINING_CKPT_DIR,SUBDATASETS_NAMES
-
-
-# Parser arguments
-config = get_config(argv=sys.argv[1:])
+# Load configuation file (conditional model)
+config = load_config("trajpred_ethucy_conditional_past.yaml")
 
 def main():
 	# Printing parameters
 	torch.set_printoptions(precision=2)
 	# Loggin format
-	logging.basicConfig(format='%(levelname)s: %(message)s',level=config.log_level)
+	logging.basicConfig(format='%(levelname)s: %(message)s',level=config["misc"]["log_level"])
 	# Device
 	if torch.cuda.is_available():
 		logging.info(torch.cuda.get_device_name(torch.cuda.current_device()))
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-	batched_train_data,batched_val_data,batched_test_data,homography,reference_image = get_dataset(config)
-	model_name    = 'deterministic'
+	batched_train_data,batched_val_data,batched_test_data,homography,reference_image = get_dataset(config["dataset"])
 
 	# Choose seed
-	torch.manual_seed(config.seed)
-	torch.cuda.manual_seed(config.seed)
-	np.random.seed(config.seed)
-	random.seed(config.seed)
+	torch.manual_seed(config["misc"]["seed"])
+	torch.cuda.manual_seed(config["misc"]["seed"])
+	np.random.seed(config["misc"]["seed"])
+	random.seed(config["misc"]["seed"])
 
 	# Model instantiation
-	model = lstm_encdec(in_size=2, embedding_dim=128, hidden_dim=128, output_size=2)
+	model = lstm_encdec(config["model"])
 	model.to(device)
 
-	# Seed for RNG
-	if config.no_retrain==False:
+	# May not have to retrain the model
+	if config["train"]["no_retrain"]==False:
 		# Train the model
-		train(model,device,0,batched_train_data,batched_val_data,config,model_name)
+		train(model,device,0,batched_train_data,batched_val_data,config)
 
 	# Load the previously trained model
-	model_filename = TRAINING_CKPT_DIR+"/"+model_name+"_"+str(SUBDATASETS_NAMES[config.id_dataset][config.id_test])+"_0.pth"
+	model_filename = config["train"]["save_dir"]+(config["train"]["model_name"].format(SUBDATASETS_NAMES[config["dataset"]["id_dataset"]][config["dataset"]["id_test"]],0))
 	logging.info("Loading {}".format(model_filename))
 	model.load_state_dict(torch.load(model_filename))
 	model.to(device)
@@ -65,7 +61,7 @@ def main():
 	ind_sample = 1
 
 	# Testing
-	for batch_idx, (datarel_test, targetrel_test, data_test, target_test) in    enumerate(batched_test_data):
+	for batch_idx, (datarel_test,targetrel_test,data_test,target_test,__,__,__) in enumerate(batched_test_data):
 		if torch.cuda.is_available():
 			datarel_test  = datarel_test.to(device)
 
@@ -75,20 +71,20 @@ def main():
 		# Plotting
 		plt.figure(figsize=(12,12))
 		plt.imshow(reference_image)
-		plot_traj_img(pred[ind_sample,:,:], data_test[ind_sample,:,:], target_test[ind_sample,:,:], homography, reference_image)
+		plot_traj_img(pred[ind_sample,:,:],data_test[ind_sample,:,:],target_test[ind_sample,:,:],homography,reference_image)
 		plt.legend()
 		plt.title('Trajectory samples {}'.format(batch_idx))
-		if config.show_plot:
+		if config["misc"]["show_test"]:
 			plt.show()
 		# Not display more than args.examples
-		if batch_idx==config.examples-1:
+		if batch_idx==config["misc"]["samples_test"]-1:
 			break
 
 	# Testing: Quantitative
 	ade  = 0
 	fde  = 0
 	total= 0
-	for batch_idx, (datavel_test, targetvel_test, data_test, target_test) in enumerate(batched_test_data):
+	for batch_idx, (datavel_test,__, data_test, target_test,__,__,__) in enumerate(batched_test_data):
 		if torch.cuda.is_available():
 			datavel_test  = datavel_test.to(device)
 		total += len(datavel_test)
