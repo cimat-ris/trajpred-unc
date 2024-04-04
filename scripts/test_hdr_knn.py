c95a14e0d166159a9b913edbc0db3441cbb12caf
Jean-Bernard Hayet <jbhayet@cimat.mx>
2024-04-03 22:01:45 -0600

    Started to integrate an efficient version of HDR with KNNs

---
+++ scripts/test_hdr_knn.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+from sklearn.datasets import make_blobs
+sys.path.append('.')
+
+from utils.hdr_knn import hdr_knn
+
+# Generated data
+centers = [[1, 1], [-1, -1], [1, -1]]
+n_samples = 500
+k         = int(np.sqrt(n_samples//2))
+
+X, labels_true = make_blobs(
+	n_samples=n_samples, centers=centers, cluster_std=0.4, random_state=0
+)
+
+# Compute the HDR
+hdr     = hdr_knn(X)
+alpha   = 0.85
+X_inside= hdr.classify(X, alpha)
+
+n_test= 1000
+Y     = np.random.uniform(low=-2, high=2, size=(n_test,2))
+Y_inside= hdr.classify(Y, alpha)
+
+
+
+plt.plot(X[X_inside==True, 0], X[X_inside==True, 1], "o",color="b",markersize=3)
+plt.plot(X[X_inside==False,0], X[X_inside==False, 1], "o",color="r",markersize=3)
+plt.plot(Y[Y_inside==True, 0],Y[Y_inside==True, 1],"o",color="c",markersize=3)
+plt.plot(Y[Y_inside==False, 0],Y[Y_inside==False, 1],"o",color="orange",markersize=3)
+# Set the aspect of the plot to be equal
+plt.axis("equal")
+plt.show()
